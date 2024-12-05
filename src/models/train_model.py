74459169c9adc82937c37dab95e4952cbe81f85e
edsanzd <eduardosanchezdata@gmail.com>
2024-12-05 16:37:45 +0100

    Updated Random Forest with n_estimators=200 and criterion='entropy'

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,6 @@
 
 import sklearn
-import pandas as pd 
+import pandas as pd
 from sklearn import ensemble
 import joblib
 import numpy as np
@@ -14,12 +14,13 @@ y_test = pd.read_csv('data/preprocessed/y_test.csv')
 y_train = np.ravel(y_train)
 y_test = np.ravel(y_test)
 
-rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
+# Updated RandomForestClassifier with new parameters
+rf_classifier = ensemble.RandomForestClassifier(n_estimators=200, criterion="entropy", n_jobs=-1, random_state=42)
 
-#--Train the model
+# Train the model
 rf_classifier.fit(X_train, y_train)
 
-#--Save the trained model to a file
+# Save the trained model to a file
 model_filename = './models/trained_model.joblib'
 joblib.dump(rf_classifier, model_filename)
 print("Model trained and saved successfully.")
